f2ca172a831accab17fec266fc4a2ca915c8835b
Andndre <agungandre687@gmail.com>
2022-12-26 20:51:45 +0800

    fix multiple fireworks spawned on other user's side when user refresh

--- backend/index.ts
+++ backend/index.ts
@@ -1,35 +1,51 @@
 import { serve } from "https://deno.land/std@0.170.0/http/mod.ts";
 
 type Player = {
-  active: boolean,
-  ws: WebSocket
-}
+  active: boolean;
+  ws: WebSocket;
+};
 
 const players: Map<string, Player> = new Map<string, Player>();
 
-type Message = {type: "launch", name: string} | {type: "join", name: string, new: boolean} | {type: "connected"} | {type: "leave", name: string} | {type: 'error', error: string};
+type Message =
+  | { type: "launch"; name: string }
+  | { type: "join"; name: string; new: boolean }
+  | { type: "connected" }
+  | { type: "leave"; name: string }
+  | { type: "error"; error: string };
 
 function onMessage(ws: WebSocket, message: Message) {
   switch (message.type) {
     case "launch":
       players.forEach((player, pName) => {
         if (message.name !== pName && player.active) {
-          player.ws.send(JSON.stringify({ type: "launch", name: message.name }));
+          player.ws.send(
+            JSON.stringify({ type: "launch", name: message.name })
+          );
         }
       });
       break;
-    case 'join':
+    case "join":
       if (players.has(message.name) && message.new) {
-        ws.send(JSON.stringify({type: 'error', error: 'name_already_exist'} as Message));
+        ws.send(
+          JSON.stringify({
+            type: "error",
+            error: "name_already_exist",
+          } as Message)
+        );
         break;
       } else if (!message.new) {
-        players.set(message.name, {active: true, ws});
-        ws.send(JSON.stringify({type: 'launch', name: message.name} as Message))
+        players.set(message.name, { active: true, ws });
+        ws.send(
+          JSON.stringify({ type: "launch", name: message.name } as Message)
+        );
       }
       players.forEach((player, name) => {
-        if (message.name !== name && player.active) {
-          player.ws.send(JSON.stringify(message ));
-          ws.send(JSON.stringify({type: 'launch', name} as Message))
+        if (message.name !== name) {
+          if (message.new && player.active) {
+            player.ws.send(JSON.stringify(message));
+          }
+          ws.send(JSON.stringify({ type: "launch", name } as Message));
         }
       });
       break;
@@ -41,12 +57,12 @@ function reqHandler(req: Request) {
     return new Response(null, { status: 501 });
   }
   const { socket: ws, response } = Deno.upgradeWebSocket(req);
-  let name = ''
+  let name = "";
   ws.onmessage = (ev: MessageEvent<string>) => {
     try {
       console.log(ev.data);
       const message = JSON.parse(ev.data) as Message;
-      if (message.type === 'join') {
+      if (message.type === "join") {
         name = message.name;
       }
       onMessage(ws, message);
@@ -55,15 +71,15 @@ function reqHandler(req: Request) {
     }
   };
   ws.onopen = () => {
-    ws.send(JSON.stringify({type: "connected"} as Message))
+    ws.send(JSON.stringify({ type: "connected" } as Message));
   };
   ws.onclose = () => {
     if (!players.has(name)) return;
     players.get(name)!.active = false;
-    
+
     players.forEach((player, pName) => {
       if (pName !== name && player.active) {
-        player.ws.send(JSON.stringify({ type: "leave", name  } as Message));
+        player.ws.send(JSON.stringify({ type: "leave", name } as Message));
       }
     });
   };
