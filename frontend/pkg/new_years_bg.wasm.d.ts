7a4c8d7d3a6a1b3231e4cbff58ca81e4fa54f277
Andndre <agungandre687@gmail.com>
2022-12-26 18:29:45 +0800

    add finished wasm

---
+++ frontend/pkg/new_years_bg.wasm.d.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function start(): void;
+export function draw(): void;
+export function spawn_firework(a: number, b: number): void;
+export function resize_canvas(): void;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export function __wbindgen_exn_store(a: number): void;
+export function __wbindgen_start(): void;
